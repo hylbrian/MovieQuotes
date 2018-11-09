e02a616232bfdf7555b70fed4994ef28cc5b726b
Brian Hyland <39333309+hylbrian@users.noreply.github.com>
2018-11-09 12:34:54 +0000

    ADDED FIREBASE FUNCTIONS

--- app/src/main/java/ie/ul/brianhyland/moviequotes/MainActivity.java
+++ app/src/main/java/ie/ul/brianhyland/moviequotes/MainActivity.java
@@ -1,15 +1,32 @@
 package ie.ul.brianhyland.moviequotes;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int mTempCounter = 0;
+    public static final String TAG = "MQ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +35,45 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //temp 
+        /*temp
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("moviequotes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });*/
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                Snackbar.make(view, "aDDED FIRESTORE DOC", Snackbar.LENGTH_LONG)
+                        .show();
+
+                /* Create a new mq with a first and last name
+                Map<String, Object> mq = new HashMap<>();
+                mTempCounter = mTempCounter +1;
+                mq.put("movie", "Quote #" + mTempCounter);
+                mq.put("quote", "Movie #" + mTempCounter);
+
+
+// Add a new document with a generated ID
+                db.collection("moviequotes").add(mq);*/
+
+                            }
+                        });
+
+
     }
 
     @Override
